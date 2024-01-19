a4d24c7876d4c9eaabd5b4f6af027c96cda97467
satyamrathore93 <129662570+satyamrathore93@users.noreply.github.com>
2024-01-19 19:24:07 +0530

    commit

---
+++ textutils/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+let name ="sid";
+function App() {
+  return (
+    <>
+      <nav>
+        <li> home </li>
+        <li>about</li>
+        <li>contact</li>
+      </nav>
+      <div className="container">
+      <h1>hello {name}</h1>
+        <p> beatae repellendus blanditiis vel quia sed voluptas itaque, maxime dolor eaque perferendis mi Voluptates, non.</p>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
